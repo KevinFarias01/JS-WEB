fb37abe1de65425da9210491363f5f24c0e88954
Kevin Farias <kevin.farias.rib@gmail.com>
2022-03-17 10:03:08 -0300

    2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 // Teclas Audio Pom 1.0
 
-
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-function tocaSom() {
-
-}
-
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+// ''para cada'' = for 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
 
+    const instrumento = tecla.classList[1];
 
-let contador = 0;
+    // Template String= usar crase + cifrão-chaves
+    const idAudio = `#som_${instrumento}`;
 
-//enquanto O contador for menor que o tamanho da lista 'length'
-while (contador < listaDeTeclas.length) {
+    //função anônima
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    //quando a tecla é apertada =
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador = contador + 1;
-
-    console.log(contador);
 };
+
+
